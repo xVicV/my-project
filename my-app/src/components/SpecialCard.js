0b0110aba681dc61956be149e3b244c3caf62c46
xVicV <towinx@gmail.com>
2023-06-30 20:45:08 +0300

    json added

--- my-app/src/components/SpecialCard.js
+++ my-app/src/components/SpecialCard.js
@@ -1,45 +1,37 @@
-import React from 'react'
-import "../styles/SpecialCard.css"
-import Dessert1 from "../assets/greeksalad.jpg"
-import Dessert2 from "../assets/bruchetta.svg"
-import Dessert3 from "../assets/lemondessert.jpg"
-import { MdDeliveryDining } from "react-icons/md"
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { MdDeliveryDining } from "react-icons/md";
+import { NavLink } from 'react-router-dom';
+import specialMenusData from '../json/specialMenusData.json';
+import Dessert1 from "../assets/greeksalad.jpg";
+import Dessert2 from "../assets/bruchetta.svg";
+import Dessert3 from "../assets/lemondessert.jpg";
+import "../styles/SpecialCard.css";
 
 const SpecialsCard = () => {
-   const specialMenus = [
-      {
-         image: Dessert1,
-         title: "Greek Salad",
-         price: "$12.99",
-         description: "The famous Greek salad of crispy lettuce, peppers, olives and our Chicago styled feta cheese, garnished with crunchy garlic, rosemary croutons.",
-         order: "Order a delivery"
-      },
-      {
-         image: Dessert2,
-         title: "Bruschetta",
-         price: "$5.99",
-         description: "Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil; made perfect for an evening dinner.",
-         order: "Order a delivery"
-      },
-      {
-         image: Dessert3,
-         title: "Lemon Dessert",
-         price: "$5.00",
-         description: "This comes straight from Grandma's recipe book. Every last ingredient has been sourced and is as authentic as can be imagined.",
-         order: "Order a delivery"
+   const specialMenusItems = specialMenusData.map(menu => {
+      let image;
+      switch (menu.image) {
+         case "greeksalad.jpg":
+            image = Dessert1;
+            break;
+         case "bruchetta.svg":
+            image = Dessert2;
+            break;
+         case "lemondessert.jpg":
+            image = Dessert3;
+            break;
+         default:
+            image = null;
+            break;
       }
-   ]
 
-   const specialMenusItems = specialMenus.map(menu => {
-      const image = menu.image;
       const title = menu.title;
       const price = menu.price;
       const description = menu.description;
       const order = menu.order;
 
       return (
-         <div>
+         <div key={title}>
             <div className="special-card-container">
                <div className="image-container">
                   <img src={image} alt="Special dessert" />
@@ -53,21 +45,22 @@ const SpecialsCard = () => {
                   <NavLink to="#">
                      <h4>
                         {order}
-                        {<MdDeliveryDining size={30} style={{ color: "#333333", marginLeft: "10px", }} />}
+                        {<MdDeliveryDining size={30} style={{ color: "#333333", marginLeft: "10px" }} />}
                      </h4>
                   </NavLink>
                </div>
             </div>
          </div>
-      )
-   })
+      );
+   });
+
    return (
       <div>
          <div className="special-card">
             {specialMenusItems}
          </div>
       </div>
-   )
+   );
 }
 
-export default SpecialsCard
+export default SpecialsCard;
--- my-app/src/components/TestimonialCard.js
+++ my-app/src/components/TestimonialCard.js
@@ -1,10 +1,10 @@
-import Star from '../assets/icon _eye_.svg';
+import ImageEye from '../assets/icon _eye_.svg';
 import "../styles/Testimonials.css";
 
 export default function TestimonialCard(props) {
    return (
       <article className="testimonial-card">
-         <img src={Star} alt="delivery Icon"></img>
+         <img src={ImageEye} alt="delivery Icon"></img>
          <h3>{props.name}</h3>
          <p>{props.description}</p>
       </article>
